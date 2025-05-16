561eb82c73db7a181ea5da608bfbd18f4d83a729
dhurvapatil <dhruvapatil871@gmail.com>
2025-05-16 16:14:25 +0530

    asdfad

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -12,7 +12,8 @@ path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+# Set the settings module to point to the inner ecommerce directory settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.ecommerce.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application() 
